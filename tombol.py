8a3e76c4b85204538f948031f83a7565ec0a373e
REYDENXX <70263982+REYDENXX@users.noreply.github.com>
2020-10-07 14:43:48 +0800

    Add files via upload
    Untuk menjalankan nya.
    pkg install git
    
    git clone https://github.com/REYDENXX/memunculkan-tombol
    
    pkg install python
    python tombol.py

---
+++ tombol.py
@@ -0,0 +1,37 @@
+import os
+from time import sleep
+#sGs
+
+a ='\033[92m'
+b ='\033[91m'
+c ='\033[0m'
+
+def setup():
+    try:
+        os.mkdir('/data/data/com.termux/files/home/.termux')
+    except:
+        pass
+    key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+    open('/data/data/com.termux/files/home/.termux/termux.properties','w').write(key)
+    os.system('termux-reload-settings')
+
+
+def banner():
+    os.system('clear')
+    print(a+'Telah Berhasil Memunculkan tombol'.center(40))
+    print(a+'Bay. Reyden x'.center(40))
+    print("".join([i for i in "\n"*3]))
+
+
+if __name__=='__main__':
+    banner()
+    from threading import Thread as td
+    t = td(target=setup)
+    t.start()
+    while t.is_alive():
+        for i in "-\|/-\|/":
+            print('\rPlease wait '+i+' ',end="",flush=True)
+            sleep(0.1)
+
+    banner()
+
